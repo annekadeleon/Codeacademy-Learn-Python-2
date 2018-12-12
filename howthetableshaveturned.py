abb9f707498727f02c7b39e5279697bdd31c137a
Anneka de Leon <annsophdl@gmail.com>
2018-12-12 14:33:57 +0800

    Create howthetableshaveturned.py

---
+++ howthetableshaveturned.py
@@ -0,0 +1,16 @@
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5
+
+# Make me false!
+bool_two = 1 >= 2
+
+# Make me true!
+bool_three = 3**3 != 9
+
+# Make me false!
+bool_four = 4/4 == 2
+
+# Make me true!
+bool_five = 10/5 > 1
