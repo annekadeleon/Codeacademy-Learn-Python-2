13723363349793ecdd9160aececc7b79f022ad34
Anneka de Leon <annsophdl@gmail.com>
2018-12-12 15:45:42 +0800

    Create and.py

---
+++ and.py
@@ -0,0 +1,14 @@
+# False and False
+bool_one = 1 == 3  and 2 != 2
+
+# -(-(-(-2))) and 4 >= 16 ** 0.5
+bool_two = False and True
+
+# 19 % 4 != 300 / 10 / 10 and False
+bool_three = True and 1 < 0
+
+# -(1 ** 2) < 2 ** 0 and 10 % 10 <= 20 - 10 * 2
+bool_four = True and True
+
+# True and True
+bool_five = 1 <= 2 and 5 > 4
