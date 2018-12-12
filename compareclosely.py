677e8acc31a5fb942786b0968a49299452df6ff4
Anneka de Leon <annsophdl@gmail.com>
2018-12-12 14:04:29 +0800

    Create compareclosely.py

---
+++ compareclosely.py
@@ -0,0 +1,14 @@
+# Set this to True if 17 < 328 or to False if it is not.
+bool_one = True
+
+# Set this to True if 100 == (2 * 50) or to False otherwise.
+bool_two = True
+
+# Set this to True if 19 <= 19 or to False if it is not.
+bool_three = True
+
+# Set this to True if -22 >= -18 or to False if it is not.
+bool_four = False
+
+# Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = False
