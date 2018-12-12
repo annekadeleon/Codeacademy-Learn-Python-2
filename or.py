bfdc3c37a530e1586a9b14689427bb6828ce6a3f
Anneka de Leon <annsophdl@gmail.com>
2018-12-12 16:14:25 +0800

    Create or.py

---
+++ or.py
@@ -0,0 +1,14 @@
+# (True or False) = True
+bool_one = 2 ** 3 == 108 % 100 or 'Cleese' == 'King Arthur'
+
+# (True or False) =
+bool_two = 'yes' == 'yes' or 'no' == 'nope'
+
+# (False or False) = False
+bool_three = 100 ** 0.5 >= 50 or 5 == 2
+
+# (True or True) = True
+bool_four = 'true' == 'true' or 'false' != 'true'
+
+# (False or False) = False
+bool_five = 1 ** 100 == 100 ** 1 or 3 * 2 * 1 != 3 + 2 + 1
