1cf2970e21d69f28ea1cef4b748dbf1dc91c348a
Anneka de Leon <annsophdl@gmail.com>
2018-12-13 23:31:13 +0800

    Create not.py

---
+++ not.py
@@ -0,0 +1,14 @@
+# not True
+bool_one = not 1 < 2
+
+# 3 ** 4 < 4** 3
+bool_two = not False
+
+#not 10 % 3 <= 10 % 2
+bool_three = not False
+
+#not 3 ** 2 + 4 ** 2 != 5 ** 2
+bool_four = not False
+
+# not not False
+bool_five = not True
