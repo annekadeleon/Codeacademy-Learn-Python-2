f06fdaf9438bfcacedd5a8571fa878056fd8ee0c
Anneka de Leon <annsophdl@gmail.com>
2018-12-15 14:56:47 +0800

    Create mixnmatch.py

---
+++ mixnmatch.py
@@ -0,0 +1,14 @@
+# Make me false!
+bool_one = (2 <= 2) and "Alpha" == "Bravo"
+
+# Make me true!
+bool_two = (1 == 1) or (2 > 1)
+
+# Make me false!
+bool_three = 3 > 4
+
+# Make me true!
+bool_four =  3 > 4 or 4 > 3
+
+# Make me true!
+bool_five = 'no' == 'no' and 'yes' == 'yes'
