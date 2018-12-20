68286f206ac2d3f6510d992084556c0f8cfbca65
Anneka de Leon <annsophdl@gmail.com>
2018-12-20 18:59:43 +0800

    Create latearrivalsandlistlength.py

---
+++ Python Lists and Dictionaries/latearrivalsandlistlength.py
@@ -0,0 +1,11 @@
+suitcase = [] 
+suitcase.append("sunglasses")
+
+suitcase.append("socks")
+suitcase.append("shirts")
+suitcase.append("pants")
+
+list_length = len(suitcase)
+
+print "There are %d items in the suitcase." % (list_length)
+print suitcase
