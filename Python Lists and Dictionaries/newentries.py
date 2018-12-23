080c7864a50cf7de01dad69c70baf0d092c0b239
Anneka de Leon <annsophdl@gmail.com>
2018-12-23 18:48:57 +0800

    Create newentries.py

---
+++ Python Lists and Dictionaries/newentries.py
@@ -0,0 +1,16 @@
+# Empty dictionary
+menu = {}
+
+# Adding new key-value pair
+menu['Chicken Alfredo'] = 14.50
+
+print menu['Chicken Alfredo']
+
+menu['Fried Chicken'] = 11.50
+
+menu['Chicken Schnitzel'] = 12.50
+
+menu['Chicken Adobo'] = 10.50
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
