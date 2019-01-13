7b0a146017b361a7fb6b3da7b9e7449353a25025
Anneka de Leon <annsophdl@gmail.com>
2019-01-13 15:29:50 +0800

    Create whileyourehere.py

---
+++ Loops/whileyourehere.py
@@ -0,0 +1,8 @@
+count = 0
+
+if count < 5:
+	print("Hello, I am an if statement and count is " + str(count))
+
+while count <= 9:
+	print("Hello, I am a whle and count is " + str(count))
+	count += 1
