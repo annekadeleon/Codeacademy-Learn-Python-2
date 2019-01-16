79f994f58a63f211a5e96430b7cb0c89689a1d80
Anneka de Leon <annsophdl@gmail.com>
2019-01-16 14:05:47 +0800

    Create countingasyougo.py

---
+++ Loops/countingasyougo.py
@@ -0,0 +1,7 @@
+choices = ["pizza", "pasta", "salad", "nachos"]
+
+print("Your choices are:")
+
+#prints and counts elements in choices
+for index, choice in enumerate(choices):
+  print index + 1, choice
