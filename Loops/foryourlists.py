69cd3e177f1e4b94a1e2823ca38989361cbda59f
Anneka de Leon <annsophdl@gmail.com>
2019-01-16 13:52:17 +0800

    Create foryourlists.py

---
+++ Loops/foryourlists.py
@@ -0,0 +1,11 @@
+numbers = [7, 9, 12, 54, 99]
+
+print("This list contains: ")
+
+#prints elements in list
+for num in numbers:
+	print num
+
+#prints each element squared
+for num in numbers:
+	print num ** 2
