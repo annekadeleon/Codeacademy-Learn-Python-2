7ca3b551d5fdddfd52cabd084628ef09a848ea0f
Anneka de Leon <annsophdl@gmail.com>
2019-01-16 20:04:14 +0800

    Update digit_sum.py

--- Practice Makes Perfect/digit_sum.py
+++ Practice Makes Perfect/digit_sum.py
@@ -1,1 +1,10 @@
 def digit_sum(n):
+	total = 0
+	string_n = str(n)
+	for char in string_n:
+		total += int(char)
+	return total
+
+pos_int = raw_input("Enter a positive integer: ")
+
+print digit_sum(pos_int)
