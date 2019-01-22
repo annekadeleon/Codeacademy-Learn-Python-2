199cb45b351b39cfe239bb0acbea70ec58e9609e
Anneka de Leon <annsophdl@gmail.com>
2019-01-22 20:58:50 +0800

    Update listcomprehensionsyntax.py

--- Advanced Topics in Python/listcomprehensionsyntax.py
+++ Advanced Topics in Python/listcomprehensionsyntax.py
@@ -4,4 +4,5 @@ doubles_by_3 = [x * 2 for x in range(1, 6) if (x * 2) % 3 == 0]
 #takes integers from 1-11, takes its square, and if the square is divisible by 2, it is put into even_sqaures list
 even_squares = [i ** 2 for i in range(1, 12) if i % 2 == 0]
 
+#prints [4, 16, 36, 64, 100]
 print even_squares
