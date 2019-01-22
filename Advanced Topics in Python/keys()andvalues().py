460f74c740714f5db65c8f0d4ecce68d271dd4fd
Anneka de Leon <annsophdl@gmail.com>
2019-01-22 17:23:40 +0800

    Create keys()andvalues().py

---
+++ Advanced Topics in Python/keys()andvalues().py
@@ -0,0 +1,11 @@
+my_dict = {
+"Name": "Anneka",
+"Age": 19,
+"BDFL": False
+}
+
+#returns list of dictionary's keys
+print my_dict.keys()
+
+#returns list of dictionary's values
+print my_dict.values()
