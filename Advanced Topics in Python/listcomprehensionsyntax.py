aa88e299412cf26fa6329d651b983494e5d68c14
Anneka de Leon <annsophdl@gmail.com>
2019-01-22 20:56:59 +0800

    Create listcomprehensionsyntax.py

---
+++ Advanced Topics in Python/listcomprehensionsyntax.py
@@ -0,0 +1,7 @@
+#takes integers from 1-5, multiplies it by 2, if the product is a multiple of 3, it is put into doubles_by_3 list
+doubles_by_3 = [x * 2 for x in range(1, 6) if (x * 2) % 3 == 0]
+
+#takes integers from 1-11, takes its square, and if the square is divisible by 2, it is put into even_sqaures list
+even_squares = [i ** 2 for i in range(1, 12) if i % 2 == 0]
+
+print even_squares
