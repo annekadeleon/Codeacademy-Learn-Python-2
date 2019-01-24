f740e4087052b2d4bf62bb84a861019f30c0c610
Anneka de Leon <annsophdl@gmail.com>
2019-01-24 20:26:02 +0800

    Create stridelength.py

---
+++ Advanced Topics in Python/stridelength.py
@@ -0,0 +1,8 @@
+#list of numbeers from 0 to 101
+to_one_hundred = range(101)
+
+#every 10th number in to_one_hundred in reversed order
+backwards_by_tens = to_one_hundred[::-10]
+
+#prints [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0]
+print backwards_by_tens
