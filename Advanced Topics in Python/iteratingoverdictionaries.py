c637eaf4676dc609341c2a2c0d39300953e68b71
Anneka de Leon <annsophdl@gmail.com>
2019-01-25 20:24:06 +0800

    Create iteratingoverdictionaries.py

---
+++ Advanced Topics in Python/iteratingoverdictionaries.py
@@ -0,0 +1,8 @@
+movies = {
+	"Monty Python and the Holy Grail": "Great",
+	"Monty Python's Life of Brian": "Good",
+	"Monty Python's Meaning of Life": "Okay"
+}
+
+#prints everything in the movies dictionary
+print movies.items()
