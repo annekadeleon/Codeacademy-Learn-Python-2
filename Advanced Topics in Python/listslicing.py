cd1fbfb2e71a43394e3aca6f8009e2e27a3352cd
Anneka de Leon <annsophdl@gmail.com>
2019-01-25 20:43:02 +0800

    Create listslicing.py

---
+++ Advanced Topics in Python/listslicing.py
@@ -0,0 +1,7 @@
+garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+
+#takes every other character in garbled and makes it reverse
+message = garbled[::-2]
+
+#prints I am the secret message!
+print message
