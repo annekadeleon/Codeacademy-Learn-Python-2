d0322f2efe94c18b3d1d1a05577eec16c9e776a6
Anneka de Leon <annsophdl@gmail.com>
2019-01-26 21:22:30 +0800

    Create icancountto1100!.py

---
+++ Introduction to Bitwise Operators/icancountto1100!.py
@@ -0,0 +1,41 @@
+#base 10 = base 2 (binary)
+# 8 bits | 4 bits | 2 bits | 1 bit
+#		1		 |		0		|		1	   |	 1
+#		8		 +		0		+		2		 +	 1 = 11
+# eleven (base 10) = 1011 (binary)
+
+# one (base 10) = 0001 (binary)
+one = 0b1
+
+# two = 0010
+two = 0b10
+
+# three = 0011
+three = 0b011
+
+# 4 = 0100
+four = 0b100
+
+# 5 = 0101
+five = 0b101
+
+# 6 = 0110
+six = 0b110
+
+# 7 = 0111
+seven = 0b111
+
+# 8 = 1000
+eight = 0b1000
+
+# 9 = 1001
+nine = 0b1001
+
+# 10 = 1010
+ten = 0b1010
+
+# 11 = 1011
+eleven = 0b1011
+
+# 12 = 1100
+twelve = 0b1100
