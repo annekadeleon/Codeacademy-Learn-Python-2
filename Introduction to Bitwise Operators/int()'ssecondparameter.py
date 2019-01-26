ce346c7aa89e53a1896e50fa0c616d00824ce637
Anneka de Leon <annsophdl@gmail.com>
2019-01-26 22:40:16 +0800

    Create int()'ssecondparameter.py

---
+++ Introduction to Bitwise Operators/int()'ssecondparameter.py
@@ -0,0 +1,19 @@
+#in int(x,y), turns x into its value in the base y
+
+#prints 1
+print int("1",2)
+
+#prints 2
+print int("10",2)
+
+#prints 7
+print int("111",2)
+
+#prints 4
+print int("0b100",2)
+
+#prints 5
+print int(bin(5),2)
+
+#prints 201
+print int("0b11001001",2)
