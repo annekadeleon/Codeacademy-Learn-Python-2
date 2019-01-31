ba862d900897af5d02399497a896895c41f27e3a
Anneka de Leon <annsophdl@gmail.com>
2019-01-31 17:20:19 +0800

    Create amethodicalapproach.py

---
+++ Introduction to Classes/amethodicalapproach.py
@@ -0,0 +1,18 @@
+class Animal(object):
+	"""Makes cute animals."""
+	#member variable is_alive is only available to members of the Animal class
+	is_alive = True
+	#__init__() is a function in the class Animal, so it is a method
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+	def description(self):
+		print self.name
+		print self.age
+
+#hippo is an instance variable of the Animal clas
+hippo = Animal("Gloria", 20)
+#calls description method
+#outputs: Gloria
+				#	20
+hippo.description()
