0ddc4bbfb1a9e4e32f8f1dbe3d8c7f935e9b177d
Anneka de Leon <annsophdl@gmail.com>
2019-01-31 19:35:22 +0800

    Create classbasics.py

---
+++ Introduction to Classes/classbasics.py
@@ -0,0 +1,7 @@
+#class called Triangle inherits object
+class Triangle(object):
+	#method __init__() initialises objects
+	def __init__(self, angle1, angle2, angle3):
+		self.angle1 = angle1
+		self.angle2 = angle2
+		self.angle3 = angle3
