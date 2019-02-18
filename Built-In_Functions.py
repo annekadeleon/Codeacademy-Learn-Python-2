4c6be0ea93cf9fcc9d6c9e143e41ac45425f152d
Anneka de Leon <annsophdl@gmail.com>
2019-02-18 20:58:43 +0800

    Update Built-In_Functions.py

--- Built-In_Functions.py
+++ Built-In_Functions.py
@@ -1,5 +1,10 @@
+#function distance_from_zero takes one argument args
 def distance_from_zero(args):
+	#if args is an integer or float type
 	if (type(args) == int) or (type(args) == float):
+		#returns absolute value (distance from zero) of args
 		return abs(args)
+	#if args is not an integer or float type
 	else:
+		#returns "Nope"
 		return("Nope")
